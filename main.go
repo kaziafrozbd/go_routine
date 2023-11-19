4cd27dfb03cf8427447fbc7684a0d267ef203b95
Kazi_Afroz <k.afroz.bd@gmail.com>
2023-11-19 08:32:02 +0600

    go routine

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	go cooking("rice")
+	cooking("curry")
+	// time.Sleep(time.Second*10)
+
+}
+func cooking(msg string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(msg, i)
+		time.Sleep(time.Second)
+	}
+}
